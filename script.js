cd11b30061c8bd2978456828f20013786ecfe6d7
odadki <93558662+odadki@users.noreply.github.com>
2022-07-25 15:53:10 -0700

    updated script.js

--- script.js
+++ script.js
@@ -17,30 +17,30 @@ var typed = new Typed(".text-animation", options);
 
 // ---------------CONTACT SUBMIT BUTTON
 
-let formSubmit = document.getElementById("form-id");
-// let contactInput = document.querySelector(".form-control");
+// let formSubmit = document.getElementById("form-id");
+// // let contactInput = document.querySelector(".form-control");
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  let name = document.getElementById("name-id");
-  let email = document.getElementById("email-id");
-  let message = document.getElementById("message-id");
+// const handleSubmit = (event) => {
+//   event.preventDefault();
+//   let name = document.getElementById("name-id");
+//   let email = document.getElementById("email-id");
+//   let message = document.getElementById("message-id");
 
-  if (
-    name.value.length > 0 &&
-    email.value.length > 0 &&
-    message.value.length > 0
-  ) {
-    let contactSubmissionForm = document.getElementById("contact-notification");
-    contactSubmissionForm.classList.add("contact-notification-class-success");
-    contactSubmissionForm.innerHTML =
-      "Thank you for your submission! I'll be in contact with you shortly.";
-    name.value = "";
-    email.value = "";
-    message.value = "";
-  } else {
-    return;
-  }
-};
-// contactInput.addEventListener("keypress", contactInputValue);
-formSubmit.addEventListener("submit", handleSubmit);
+//   if (
+//     name.value.length > 0 &&
+//     email.value.length > 0 &&
+//     message.value.length > 0
+//   ) {
+//     let contactSubmissionForm = document.getElementById("contact-notification");
+//     contactSubmissionForm.classList.add("contact-notification-class-success");
+//     contactSubmissionForm.innerHTML =
+//       "Thank you for your submission! I'll be in contact with you shortly.";
+//     name.value = "";
+//     email.value = "";
+//     message.value = "";
+//   } else {
+//     return;
+//   }
+// };
+// // contactInput.addEventListener("keypress", contactInputValue);
+// formSubmit.addEventListener("submit", handleSubmit);
