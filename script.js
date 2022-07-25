b7fa7f85ceaba0c4760e8811952b8766033b1273
odadki <93558662+odadki@users.noreply.github.com>
2022-07-25 15:42:12 -0700

    updated contact form

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// ------------LANDING PAGE
 var options = {
   strings: [
     "^3000 hiking 🌿",
@@ -13,3 +14,33 @@ var options = {
 };
 
 var typed = new Typed(".text-animation", options);
+
+// ---------------CONTACT SUBMIT BUTTON
+
+let formSubmit = document.getElementById("form-id");
+// let contactInput = document.querySelector(".form-control");
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  let name = document.getElementById("name-id");
+  let email = document.getElementById("email-id");
+  let message = document.getElementById("message-id");
+
+  if (
+    name.value.length > 0 &&
+    email.value.length > 0 &&
+    message.value.length > 0
+  ) {
+    let contactSubmissionForm = document.getElementById("contact-notification");
+    contactSubmissionForm.classList.add("contact-notification-class-success");
+    contactSubmissionForm.innerHTML =
+      "Thank you for your submission! I'll be in contact with you shortly.";
+    name.value = "";
+    email.value = "";
+    message.value = "";
+  } else {
+    return;
+  }
+};
+// contactInput.addEventListener("keypress", contactInputValue);
+formSubmit.addEventListener("submit", handleSubmit);
